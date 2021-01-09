fc6449d554ef8ee03507c8f9dc342749440f6408
Vadim-Kononov <65465568+Vadim-Kononov@users.noreply.github.com>
2021-01-09 20:01:28 +0300

    Delete Account.h

--- ESP32_Outdoor/Account.h
+++
@@ -1,6 +0,0 @@
-/*Символьные константы для MQTT*/
-
-/*Символьные константы для IFTTT*/
-/*Аккаунт vadim.kononov.net@gmail.com*/
-const char * ifttt_event	  PROGMEM = "climate";
-const char * ifttt_api_key	PROGMEM = "nxDul7UGF8ulaxG3tXgKU2iQSCYkRlJvZ_5pZdC2UX0";
